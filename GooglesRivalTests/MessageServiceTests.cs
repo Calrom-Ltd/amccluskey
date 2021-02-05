a594e0817e1d93bc171083950ffde727ee1c5740
Adam McCluskey <aMcCluskey@calrom.com>
2021-02-05 14:33:17 +0000

    #11 initial commit

---
+++ GooglesRivalTests/MessageServiceTests.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GooglesRival.Services;
+
+namespace GooglesRivalTests
+{
+    [TestClass]
+    public class MessageServiceTests
+    {
+        /// <summary>
+        /// Verifies the messages exist.
+        /// </summary>
+        [TestMethod]
+        public void VerifyMessagesExist()
+        {
+            var message = new MessageService();
+            Assert.IsNotNull(message.messages);
+            Assert.AreEqual(16, message.messages.Count,  "The message count should match");
+        }
+
+        /// <summary>
+        /// Verifies the message expected is returned.
+        /// </summary>
+        [TestMethod]
+        public void VerifyMessageExpectedIsReturned()
+        {
+            var message = new MessageService();
+
+            var sut = message.GetMessageById("1");
+
+            Assert.IsNotNull(sut);
+            Assert.AreEqual(1, sut.Id, "The returned message should have the same ID");
+        }
+    }
+}
