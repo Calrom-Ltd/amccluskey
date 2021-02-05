5a219604526146bf3720bc175d3daa6b4bae8126
Adam McCluskey <aMcCluskey@calrom.com>
2021-02-05 17:22:12 +0000

    message service tests

--- GooglesRivalTests/MessageServiceTests.cs
+++ GooglesRivalTests/MessageServiceTests.cs
@@ -1,34 +1,93 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using GooglesRival.Services;
+using GooglesRival.Models;
+using System;
 
 namespace GooglesRivalTests
 {
     [TestClass]
     public class MessageServiceTests
     {
-        /// <summary>
-        /// Verifies the messages exist.
-        /// </summary>
+
         [TestMethod]
-        public void VerifyMessagesExist()
+        [TestCategory("Demo")]
+        public void VerifyMessagesDetailsCorrect()
         {
-            var message = new MessageService();
-            Assert.IsNotNull(message.messages);
-            Assert.AreEqual(16, message.messages.Count,  "The message count should match");
+            //// Setup
+            var messageService = new MessageService();
+            Message message = new Message()
+            {
+                Username = "User0",
+                Date = DateTime.Now,
+                Subject = "Hey Kids, have you heard of Rick rolling?",
+                body = "Haha Press F to Pay respects :-P",
+            };
+            //// Act
+            var sut = messageService.GetMessageById("0");
+
+            //// Assert
+            Assert.IsNotNull(sut);
+            Assert.AreEqual(message.Id, sut.Id);
+            Assert.AreEqual(message.Username, sut.Username);
+            Assert.AreEqual(message.Subject, sut.Subject);
+            Assert.AreEqual(message.body, sut.body);
         }
 
         /// <summary>
         /// Verifies the message expected is returned.
         /// </summary>
         [TestMethod]
-        public void VerifyMessageExpectedIsReturned()
+        [TestCategory("Demo")]
+        public void VerifyMessageIsReturnedForValidUsername()
         {
-            var message = new MessageService();
+            //// Setup
+            var messageService = new MessageService();
+            Message message = new Message()
+            {
+                Username = "User0",
+                Date = DateTime.Now,
+                Subject = "Hey Kids, have you heard of Rick rolling?",
+                body = "Haha Press F to Pay respects :-P",
+            };
+            //// Act
+            var sut = messageService.GetMessagesForUser("User0");
 
-            var sut = message.GetMessageById("1");
-
+            //// Assert
             Assert.IsNotNull(sut);
-            Assert.AreEqual(1, sut.Id, "The returned message should have the same ID");
+            Assert.AreEqual(2, sut.Count);
+            Assert.AreEqual(message.Id, sut[0].Id);
+            Assert.AreEqual(message.Username, sut[0].Username);
+            Assert.AreEqual(message.Subject, sut[0].Subject);
+            Assert.AreEqual(message.body, sut[0].body);
+        }
+
+        [TestMethod]
+        [TestCategory("Demo")]
+        public void VerifyNoMessageIsReturnedForInValidUsername()
+        {
+            //// Setup
+            var messageService = new MessageService();
+
+            //// Act
+            var sut = messageService.GetMessagesForUser("FakeUserName");
+
+            //// Assert
+            Assert.IsNotNull(sut);
+            Assert.AreEqual(0, sut.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("Demo")]
+        public void VerifyNoMessageIsReturnedForInValidMessageID()
+        {
+            //// Setup
+            var messageService = new MessageService();
+
+            //// Act
+            var sut = messageService.GetMessageById("-1");
+
+            //// Assert
+            Assert.IsNull(sut);
         }
     }
 }
