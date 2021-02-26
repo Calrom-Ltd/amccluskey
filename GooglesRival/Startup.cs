cd3e284e25b20eec7822df100956128637d95896
Adam McCluskey <aMcCluskey@calrom.com>
2021-02-26 10:16:29 +0000

    Add ExcludeFromCodeCoverage from startup and program remove unused usings

--- GooglesRival/Startup.cs
+++ GooglesRival/Startup.cs
@@ -1,22 +1,17 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using GooglesRival.Services;
 using GooglesRival.Services.Iservices;
 using GooglesRival.Controllers;
+using System.Diagnostics.CodeAnalysis;
 
 namespace GooglesRival
 {
+    [ExcludeFromCodeCoverage]
     public class Startup
     {
         public Startup(IConfiguration configuration)
