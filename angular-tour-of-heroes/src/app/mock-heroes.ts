2846791cacfa6b96fa8a3cc3dfca2eaef7451db7
Adam McCluskey <aMcCluskey@calrom.com>
2021-03-24 13:18:37 +0000

    Add Tour of heroes

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'D.Va' },
+  { id: 12, name: 'Reinheart' },
+  { id: 13, name: 'Orisa' },
+  { id: 14, name: 'Sigma' },
+  { id: 15, name: 'Roadhog' },
+  { id: 15, name: 'Winston' }
+];
